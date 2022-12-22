92311b40d89d781f6f54c592f2c96b46e0b8573e
ISHMEET2 <97910350+ISHMEET2@users.noreply.github.com>
2022-12-22 06:03:57 +0530

    Add files via upload

---
+++ build/precache-manifest.29be10e5838ba6a72cd7dacfbbe13a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e08340451134eb59aadf",
+    "url": "/its_react/static/css/main.3b28e586.chunk.css"
+  },
+  {
+    "revision": "e08340451134eb59aadf",
+    "url": "/its_react/static/js/main.4aa60c54.chunk.js"
+  },
+  {
+    "revision": "d3093d7469416caa465d",
+    "url": "/its_react/static/js/runtime~main.c6ca6edd.js"
+  },
+  {
+    "revision": "ef5a974e2ed10d5dc2fb",
+    "url": "/its_react/static/js/2.2aaa68ec.chunk.js"
+  },
+  {
+    "revision": "78a939d5c711cd34a2e1",
+    "url": "/its_react/static/js/3.1f0a8b36.chunk.js"
+  },
+  {
+    "revision": "cb798aca2b626a06e83745d513bae22c",
+    "url": "/its_react/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+//<><script src="/static/js/1.3djhfha.chunk.js"></script><script src="/static/js/main.detdjhs0.chunk.js"></script></>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
